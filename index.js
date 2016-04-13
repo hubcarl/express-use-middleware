544ed90b65569c14410ba71c82697b5376d1e627
sky <cl98914@alibaba-inc.com>
2016-04-13 22:46:35 +0800

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+/** 
+ * index 
+ * Description：根据配置文件自动注册express中间件
+ * Created by sky on 16/4/13
+ * Copyright (c) 2016 sky All Rights Reserved
+ */
+
+module.exports = require('./lib/middleware.js');
